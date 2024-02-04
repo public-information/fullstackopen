496202f12f4a60683b95a491f414deafc01b085b
Kenneth Linehan <kwlinehan@kenneths-mbp-m1.myfiosgateway.com>
2024-02-03 22:29:48 -0500

    Full Stack Open Part 1d Exercise 1.7: Refactor useState hook to use feedback object. Refactor onClick handler to handle all feedback categories using object spread syntax. See: handleFeedback. Create statistics functions for calculating all, average score and positive percentage. Add new statistics Counter components to App. Create separate Percentage component, to accomodate % suffix on number. Add Percentage component to App

--- exercises/parts-1-a--1-d/unicafe/src/App.jsx
+++ exercises/parts-1-a--1-d/unicafe/src/App.jsx
@@ -1,58 +1,95 @@
 import { useState } from 'react'
 
-const Heading = (props) => {
+const Heading = ({heading}) => {
     return (
         <>
-            <h2>{props.heading}</h2>
+            <h2>{heading}</h2>
         </>
     )
 }
 
-const Label = (props) => {
+const Label = ({label}) => {
     return (
         <>
-            <span>{props.label}</span>
+            <span>{label}</span>
         </>
     )
 }
 
-const Counter = (props) => {
+const Button = ({handleClick, label}) => {
     return (
         <>
-            <p><Label label={props.category} />: {props.count}</p>
+            <button onClick={handleClick}>{label}</button>
         </>
     )
 }
-const Button = (props) => {
+
+const Counter = ({label, count}) => {
     return (
         <>
-            <button onClick={props.handleClick}>{props.category}</button>
+            <p><Label label={label} />: {count}</p>
         </>
     )
 }
+
+const Percentage = ({label, percentage}) => {
+    return (
+        <>
+            <p><Label label={label} />: {percentage} %</p>
+        </>
+    )
+}
+
 const App = () => {
 
     const goodLabel = 'good'
     const neutralLabel = 'neutral'
     const badLabel = 'bad'
 
-    const [good, setGood] = useState(0)
-    const [neutral, setNeutral] = useState(0)
-    const [bad, setBad] = useState(0)
+    const [feedback, setFeedback] = useState({
+        [`${goodLabel}`]: 0,
+        [`${neutralLabel}`]: 0,
+        [`${badLabel}`]: 0,
+    })
+
+    const totalFeedback = () => {
+        return feedback[goodLabel] + feedback[neutralLabel] + feedback[badLabel]
+    }
+
+    const averageFeedbackScore = () => {
+        let score = feedback[goodLabel] + (feedback[badLabel] * -1)
+        let total = totalFeedback()
+        return total ? (score / total) : total
+    }
+
+    const positiveFeedbackPercentage = () => {
+        let positive = (feedback[goodLabel] / totalFeedback()) * 100
+        return totalFeedback() ? positive : totalFeedback()
+    }
+
+    const handleFeedback = (category) => () => {
+            setFeedback({
+                ...feedback,
+                [`${category}`]: feedback[category] + 1,
+            })
+    }
 
     return (
         <div>
             <div>
                 <Heading heading={"give feedback"} />
-                <Button handleClick={() => setGood(good + 1)} category={goodLabel} />
-                <Button handleClick={() => setNeutral(neutral + 1)} category={neutralLabel} />
-                <Button handleClick={() => setBad(bad + 1)} category={badLabel} />
+                <Button handleClick={handleFeedback(goodLabel)} label={goodLabel} />
+                <Button handleClick={handleFeedback(neutralLabel)} label={neutralLabel} />
+                <Button handleClick={handleFeedback(badLabel)} label={badLabel} />
             </div>
             <div>
                 <Heading heading={"statistics"} />
-                <Counter category={goodLabel} count={good}/>
-                <Counter category={neutralLabel} count={neutral}/>
-                <Counter category={badLabel} count={bad}/>
+                <Counter label={goodLabel} count={feedback[goodLabel]}/>
+                <Counter label={neutralLabel} count={feedback[neutralLabel]}/>
+                <Counter label={badLabel} count={feedback[badLabel]}/>
+                <Counter label={'all'} count={totalFeedback()}/>
+                <Counter label={'average'} count={averageFeedbackScore()}/>
+                <Percentage label={'positive'} percentage={positiveFeedbackPercentage()}/>
             </div>
         </div>
     )

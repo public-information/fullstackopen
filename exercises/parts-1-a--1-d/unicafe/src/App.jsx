94e71f94028053c48578685c830e4979e91a3c38
Kenneth Linehan <kwlinehan@kenneths-mbp-m1.myfiosgateway.com>
2024-02-03 20:09:48 -0500

    Full Stack Open Part 1d Exercise 1.6: Create customer feedback Button component. Add Buttons for good, neutral, bad. Create Counter component to display total for category. Add Counter for good, neutral, bad. Update statistics for categories using  onClick event handlers. Create and add Heading Components

--- exercises/parts-1-a--1-d/unicafe/src/App.jsx
+++ exercises/parts-1-a--1-d/unicafe/src/App.jsx
@@ -1,35 +1,61 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Heading = (props) => {
+    return (
+        <>
+            <h2>{props.heading}</h2>
+        </>
+    )
+}
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const Label = (props) => {
+    return (
+        <>
+            <span>{props.label}</span>
+        </>
+    )
+}
+
+const Counter = (props) => {
+    return (
+        <>
+            <p><Label label={props.category} />: {props.count}</p>
+        </>
+    )
+}
+const Button = (props) => {
+    return (
+        <>
+            <button onClick={props.handleClick}>{props.category}</button>
+        </>
+    )
+}
+const App = () => {
+
+    const goodLabel = 'good'
+    const neutralLabel = 'neutral'
+    const badLabel = 'bad'
+
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    return (
+        <div>
+            <div>
+                <Heading heading={"give feedback"} />
+                <Button handleClick={() => setGood(good + 1)} category={goodLabel} />
+                <Button handleClick={() => setNeutral(neutral + 1)} category={neutralLabel} />
+                <Button handleClick={() => setBad(bad + 1)} category={badLabel} />
+            </div>
+            <div>
+                <Heading heading={"statistics"} />
+                <Counter category={goodLabel} count={good}/>
+                <Counter category={neutralLabel} count={neutral}/>
+                <Counter category={badLabel} count={bad}/>
+            </div>
+        </div>
+    )
 }
 
 export default App

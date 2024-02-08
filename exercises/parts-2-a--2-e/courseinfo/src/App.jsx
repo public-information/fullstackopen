e9a518caa6625ac28180157b98dd6bfa160ee4ab
Kenneth Linehan <kwlinehan@kenneths-mbp-m1.myfiosgateway.com>
2024-02-07 22:21:16 -0500

    Full Stack Open Part 2a: Exercise 2.1: Create Content component. Use .map function to iterate through parts array and create n-number of Parts components from the array data. Refactor to use destructuring of all props passed to components. Add new Course component to App. Add id property to parts in array

--- exercises/parts-2-a--2-e/courseinfo/src/App.jsx
+++ exercises/parts-2-a--2-e/courseinfo/src/App.jsx
@@ -1,35 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const Header = ({name}) => {
+    return (
+        <>
+            <h1>{name}</h1>
+        </>
+    )
+}
 
-function App() {
-  const [count, setCount] = useState(0)
+const Part = ({part}) => {
+    return (
+        <>
+            <p>{part.name} {part.exercises}</p>
+        </>
+    )
+}
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const Content = ({parts}) => {
+    return (
+        <div>
+            {parts.map(part =>
+                <Part key={part.id} part={part} />
+            )}
+        </div>
+    )
+}
+
+const Course = ({course}) => {
+    return (
+        <>
+            <Header name={course.name} />
+            <Content parts={course.parts} />
+        </>
+    )
+}
+
+const App = () => {
+    const course = {
+        id: 1,
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10,
+                id: 1,
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7,
+                id: 2
+            },
+            {
+                name: 'State of a component',
+                exercises: 14,
+                id: 3
+            }
+        ]
+    }
+
+    return (
+        <div>
+            <Course course={course} />
+        </div>
+    )
 }
 
 export default App
